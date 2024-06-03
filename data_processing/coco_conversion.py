744af233e23b6d7e6261ec360b1f3d6ddee6a870
jvk-boveye <james@boveye.com>
2024-06-02 22:24:56 -0700

    updates to data processing

--- data_processing/coco_conversion.py
+++ data_processing/coco_conversion.py
@@ -1,141 +1,22 @@
-# import json
-# import os
-# import numpy as np
-# import pycocotools.mask as mask_util
-#
-#
-# def associate_data(image_dir, mask_dir):
-#     name_association = {}
-#     for file in os.listdir(image_dir):
-#         if file.endswith('.png'):
-#             # Recover image name as key
-#             image_name = os.path.splitext(file)[0]
-#             split_name = image_name.split('_')
-#             mask_filename = f'{split_name[0]}_mask_{split_name[2]}.npy'
-#             mask_path = os.path.join(mask_dir, mask_filename)
-#             name_association[image_name] = mask_path
-#     return name_association
-#
-#
-# def to_coco(name_association, class_names, size=512):
-#
-#     categories = [{'id': i, 'name': name, 'supercategory': 'none'} for i, name in enumerate(class_names, 0)]
-#
-#     coco_images = []
-#     coco_annotations = []
-#     ann_id = 0
-#
-#     for file_id, (image_name, mask_path) in enumerate(name_association.items(), 0):
-#
-#         coco_images.append({
-#             'id': file_id,
-#             'file_name': image_name,
-#             'height': size,
-#             'width': size
-#         })
-#
-#         mask = np.load(mask_path)
-#
-#         for class_id in np.unique(mask):
-#             if class_id == 0:
-#                 continue  # Skip background
-#             class_mask = (mask == class_id).astype(np.uint8)
-#             rle = mask_util.encode(np.asfortranarray(class_mask))
-#             bbox = mask_util.toBbox(rle).tolist()
-#             area = int(mask_util.area(rle))
-#
-#             coco_annotations.append({
-#                 'id': ann_id,
-#                 'image_id': image_name,
-#                 'category_id': int(class_id),
-#                 'area': area,
-#                 'bbox': bbox,
-#                 'segmentation': rle,
-#                 'iscrowd': 0
-#             })
-#
-#             ann_id += 1
-#
-#     coco_dict = {
-#         'images': coco_images,
-#         'categories': categories,
-#         'annotations': coco_annotations
-#     }
-#
-#     return coco_dict
-#
-#
-# def split_train_val(coco_dict, train_ratio, dataset_pct=1.0, seed=2013):
-#     np.random.seed(seed)
-#
-#     assert 0.0 < dataset_pct <= 1.0, "Invalid 'dataset_pct': 0 < dataset_pct <= 1"
-#
-#     num_images = len(coco_dict['images'])
-#     num_to_use = int(num_images*dataset_pct)
-#     indices = np.random.permutation(num_images)[:num_to_use]
-#
-#     train_indices = set(indices[:int(train_ratio * num_to_use)])
-#     val_indices = set(indices[int(train_ratio * num_to_use):])
-#
-#     train_images = [coco_dict['images'][i] for i in train_indices]
-#     val_images = [coco_dict['images'][i] for i in val_indices]
-#
-#     train_image_ids = {img['id'] for img in train_images}
-#     val_image_ids = {img['id'] for img in val_images}
-#
-#     train_annotations = [ann for ann in coco_dict['annotations'] if ann['image_id'] in train_image_ids]
-#     val_annotations = [ann for ann in coco_dict['annotations'] if ann['image_id'] in val_image_ids]
-#
-#     train_data = {
-#         'images': train_images,
-#         'annotations': train_annotations,
-#         'categories': coco_dict['categories']
-#     }
-#
-#     val_data = {
-#         'images': val_images,
-#         'annotations': val_annotations,
-#         'categories': coco_dict['categories']
-#     }
-#
-#     print("JSON Split into training/val Complete.\n")
-#     return train_data, val_data
-#
-#
-# def json_gen(json_dir, image_dir, mask_dir, class_names, train_ratio=0.8, dataset_pct=1.0, seed=2013, size=512):
-#     train_json = os.path.join(json_dir, 'train.json')
-#     val_json = os.path.join(json_dir, 'val.json')
-#     os.makedirs(json_dir, exist_ok=True)
-#
-#     name_association = associate_data(image_dir, mask_dir)
-#     coco_dict = to_coco(name_association, class_names, size)
-#     train_data, val_data = split_train_val(coco_dict, train_ratio, dataset_pct, seed)
-#
-#     with open(train_json, 'w') as f:
-#         json.dump(train_data, f)
-#
-#     with open(val_json, 'w') as f:
-#         json.dump(val_data, f)
-
-
 import json
 import os
 import numpy as np
 import pycocotools.mask as mask_util
 from tqdm import tqdm
+from skimage.measure import find_contours
 
 
 def associate_data(image_dir, mask_dir):
     name_association = {}
-    print("Associating image files with mask files...")
-    for file in tqdm(os.listdir(image_dir), desc="Processing files"):
+    print("  Associating image files with mask files...")
+    for file in os.listdir(image_dir):
         if file.endswith('.png'):
             # Recover image name as key
             image_name = os.path.splitext(file)[0]
             split_name = image_name.split('_')
             mask_filename = f'{split_name[0]}_mask_{split_name[2]}.npy'
             mask_path = os.path.join(mask_dir, mask_filename)
-            name_association[image_name] = mask_path
+            name_association[file] = mask_path
     return name_association
 
 
@@ -159,14 +40,24 @@ def to_coco(name_association, class_names, size=512):
 
         mask = np.load(mask_path)
 
+        # Iterate through each unique class in the mask
         for class_id in np.unique(mask):
             if class_id == 0:
                 continue  # Skip background
+            # Create a binary mask for the current class
             class_mask = (mask == class_id).astype(np.uint8)
+
+            # Encode the binary mask to RLE
             rle = mask_util.encode(np.asfortranarray(class_mask))
+
+            # Convert RLE 'counts' to an ASCII string
+            rle['counts'] = rle['counts'].decode('ascii')
+
+            # Get bounding box and area
             bbox = mask_util.toBbox(rle).tolist()
             area = int(mask_util.area(rle))
 
+            # Create annotation for the current class mask
             coco_annotations.append({
                 'id': ann_id,
                 'image_id': file_id,
@@ -177,6 +68,7 @@ def to_coco(name_association, class_names, size=512):
                 'iscrowd': 0
             })
 
+
             ann_id += 1
 
     coco_dict = {
@@ -186,6 +78,7 @@ def to_coco(name_association, class_names, size=512):
     }
 
     return coco_dict
+
 
 
 def split_train_val(coco_dict, train_ratio, dataset_pct=1.0, seed=2013):
@@ -221,7 +114,6 @@ def split_train_val(coco_dict, train_ratio, dataset_pct=1.0, seed=2013):
         'categories': coco_dict['categories']
     }
 
-    print("JSON Split into training/val Complete.\n")
     return train_data, val_data
 
 
@@ -231,14 +123,12 @@ def json_gen(json_dir, image_dir, mask_dir, class_names, train_ratio=0.8, datase
     val_json = os.path.join(json_dir, 'val.json')
     os.makedirs(json_dir, exist_ok=True)
 
-    print("Associating data...")
     name_association = associate_data(image_dir, mask_dir)
-    print("Creating COCO dictionary...")
     coco_dict = to_coco(name_association, class_names, size)
-    print("Splitting data into training and validation sets...")
+    print("  Splitting data into training and validation sets...")
     train_data, val_data = split_train_val(coco_dict, train_ratio, dataset_pct, seed)
 
-    print("Saving JSON files...")
+    print("  Saving JSON files...")
     with open(train_json, 'w') as f:
         json.dump(train_data, f)
 
--- data_processing/split.py
+++ data_processing/split.py
@@ -1,21 +1,10 @@
-# import cv2
-# import imageio
-# import math
 import os
 import numpy as np
-# import pandas as pd
-# import rasterio
 from PIL import Image
 import time
 from tqdm.notebook import tqdm
-# import geopandas as gpd
-# import matplotlib.pyplot as plt
-# import numpy as np
-# import skimage as ski
-# from rasterio import mask
-# from skimage.exposure import equalize_adapthist, adjust_gamma, rescale_intensity
 
-Image.MAX_IMAGE_PIXELS = 268468226  # For example
+Image.MAX_IMAGE_PIXELS = 268468226
 
 def save_image_tiles(input_directory, output_directory, tile_size=512):
     if not os.path.exists(output_directory):
@@ -68,66 +57,13 @@ def save_image_tiles(input_directory, output_directory, tile_size=512):
             print(f'     Elapsed Time for image = {end_time - start_time:.2f} seconds\n')
     print(f'     Image Tiling Complete.\n\n')
 
-#
-# def save_mask_tiles(input_directory, output_directory, tile_size=512, mask_values=None):
-#     if not os.path.exists(output_directory):
-#         os.makedirs(output_directory)
-#
-#     # Iterate over each image in the input directory
-#     for filename in os.listdir(input_directory):
-#         if filename.lower().endswith('tif'):
-#             input_image_path = os.path.join(input_directory, filename)
-#             # Load the image using PIL
-#             image = Image.open(input_image_path)
-#             width, height = image.size
-#
-#             # Remove the final row and column of pixels
-#             if width > 1 and height > 1:
-#                 image = image.crop((0, 0, width - 1, height - 1))
-#                 width, height = image.size
-#
-#             print(f'Standardizing Image: {filename}')
-#
-#             slice_count = 0
-#
-#             if width % tile_size != 0 and height % tile_size != 0:
-#                 raise RuntimeError("Image is not compatible with tile size")
-#
-#             # Determine the number of rows and columns needed to achieve the desired size
-#             rows = height // tile_size
-#             cols = width // tile_size
-#
-#             # Iterate over the split grid
-#             for row in range(rows):
-#                 for col in range(cols):
-#                     # Calculate the slice boundaries
-#                     x_start = col * tile_size
-#                     y_start = row * tile_size
-#                     x_end = x_start + tile_size
-#                     y_end = y_start + tile_size
-#
-#                     # Extract the slice from the image
-#                     slice_image = image.crop((x_start, y_start, x_end, y_end))
-#                     # Create a standardized base filename by removing the extension
-#                     base_filename = os.path.splitext(filename)[0]
-#                     slice_name = os.path.join(output_directory, f"{base_filename}_{slice_count:04d}.png")
-#
-#                     # Save the slice as an image file
-#                     slice_image.save(slice_name)
-#
-#                     slice_count += 1
-#
-#             print(f'     {slice_count} tiles created and saved for {filename} in {output_directory}.\n')
-#     print(f'     Mask Tiling Complete.\n\n')
-#
-
 
 def save_mask_tiles(input_directory, output_directory, tile_size=512, allowed_values=None):
     if not os.path.exists(output_directory):
         os.makedirs(output_directory)
 
-    # Iterate over each image in the input directory
-    for filename in os.listdir(input_directory):
+    # Iterate over each image in the input directory with tqdm progress bar
+    for filename in tqdm(os.listdir(input_directory), desc="Processing Images"):
         if filename.lower().endswith('tif'):
             input_image_path = os.path.join(input_directory, filename)
             # Load the image using PIL
@@ -150,36 +86,39 @@ def save_mask_tiles(input_directory, output_directory, tile_size=512, allowed_va
             rows = height // tile_size
             cols = width // tile_size
 
-            # Convert the image to a numpy array
-            image_array = np.array(image)
+            total_tiles = rows * cols
 
-            # Iterate over the split grid
-            for row in range(rows):
-                for col in range(cols):
-                    # Calculate the slice boundaries
-                    x_start = col * tile_size
-                    y_start = row * tile_size
-                    x_end = x_start + tile_size
-                    y_end = y_start + tile_size
+            # Single progress bar for both loops
+            with tqdm(total=total_tiles, desc=f"Tiling {filename}") as pbar:
+                # Iterate over the split grid
+                for row in range(rows):
+                    for col in range(cols):
+                        # Calculate the slice boundaries
+                        x_start = col * tile_size
+                        y_start = row * tile_size
+                        x_end = x_start + tile_size
+                        y_end = y_start + tile_size
 
-                    # Extract the slice from the image array
-                    slice_array = image_array[y_start:y_end, x_start:x_end]
+                        # Extract the slice from the image array
+                        slice_array = np.array(image.crop((x_start, y_start, x_end, y_end)))
 
-                    # If allowed_values is provided, set every pixel value to 0 unless it is in the allowed values list
-                    if allowed_values is not None:
-                        slice_array = np.where(np.isin(slice_array, allowed_values), slice_array, 0)
+                        # If allowed_values is provided, remap the pixel values
+                        if allowed_values is not None:
+                            remapped_slice = np.zeros_like(slice_array)
+                            for original_value, new_value in allowed_values.items():
+                                remapped_slice[slice_array == original_value] = new_value
+                            slice_array = remapped_slice
 
-                    # Convert the slice array back to an image
-                    slice_image = Image.fromarray(slice_array)
+                        # Create a standardized base filename by removing the extension
+                        base_filename = os.path.splitext(filename)[0]
+                        slice_path = os.path.join(output_directory, f"{base_filename}_{slice_count:04d}.png")
 
-                    # Create a standardized base filename by removing the extension
-                    base_filename = os.path.splitext(filename)[0]
-                    slice_name = os.path.join(output_directory, f"{base_filename}_{slice_count:04d}.png")
+                        # Save slice as PNG
+                        slice_image = Image.fromarray(slice_array.astype(np.uint8))
+                        slice_image.save(slice_path)
 
-                    # Save the slice as an image file
-                    slice_image.save(slice_name)
-
-                    slice_count += 1
+                        slice_count += 1
+                        pbar.update(1)  # Update the progress bar for each tile
 
             print(f'     {slice_count} tiles created and saved for {filename} in {output_directory}.\n')
     print(f'     Mask Tiling Complete.\n\n')
